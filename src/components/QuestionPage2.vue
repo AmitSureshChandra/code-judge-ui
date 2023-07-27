cc36a7df82dd2ab558ee3ff34d90886103cfd002
amit <akumar00029@gmail.com>
2023-07-27 21:02:08 +0530

    hard coded question detail page with code run + test cases working

--- src/components/QuestionPage2.vue
+++ src/components/QuestionPage2.vue
@@ -1,25 +1,127 @@
 <template>
-    <div class="question-container">
-        <div class="question-detail">
-             <label class="title">Problem Title:</label>
-            <div class="title-text">{{ question.title }}</div>
+    <v-app >
+        <v-row class="ma-1" style="height: 95vh; width: 100%">
+                <v-card width="30%">
+                    <v-card-title>
+                        <h4>
+                            {{ question.id }} . {{question.title}}
+                        </h4>
+                    </v-card-title>
+                    <v-card-text>
+                        <div class="my-2">
+                            <p>{{question.description}}</p>
+                        </div>
+                        <v-divider />
+                        <div>
+                            <b>Examples</b> 
+                            <v-card class="ma-1" v-for="(example, index) in question.examples" :key="index">
+                                <v-card-text>
+                                    <p>input : {{example.input }}</p>
+                                    <p>output : {{example.output }}</p>
+                                    <p>explanation : {{example.explanation }}</p>
+                                </v-card-text>
+                            </v-card>
+                        </div>
+                        <div>
+                            <b>Contraints</b> 
+                            <v-card class="ma-1" v-for="(contraint, index) in question.constraints" :key="index">
+                                <v-card-text>
+                                   <p>{{ contraint }}</p>
+                                </v-card-text>
+                            </v-card>
+                        </div>
+                    </v-card-text>
+                </v-card>
+                <v-card width="70%" class="pa-2">
+                   <v-col class="mb-2">
+                    <v-row>
+                        <v-select v-model="compiler" density="compact" max-width="200" label="compiler" :items="['jdk8','golang']" variant="solo" />
+                        <v-spacer />
+                        <v-spacer />
+                        <v-spacer />
+                        <v-spacer />
+                    </v-row>
+                     <v-row>
+                        <v-textarea v-model="code" auto-grow rows="10">
+                        </v-textarea>
+                    </v-row>
+                   
+                    <v-spacer/>
+                    <v-row >
+                        <v-card class="" width="100%">
+                            <v-tabs
+                            dark
+                            v-model="code_tab"
+                            
+                            >
+                            <v-tab value="test_cases">Test Cases</v-tab>
+                            <v-tab value="result">Result</v-tab>
+                            </v-tabs>
 
-            <label class="title">Problem Description:</label>
-            <div class="description">{{  question.description }}</div>
-        </div>
-        <div class="code-container">
-            <div class="code-block">
-                <input type="text" class="code-input">
-            </div>
-            <div class="console-">
+                            <v-card-text>
+                            <v-window v-model="code_tab">
+                                <v-window-item value="test_cases">
+                                    <v-tabs
+                                    dark
+                                    v-model="testcase_tab"
+                                    
+                                    >
+                                        <v-tab v-for="(test_case, i) in question.testCases" :key="i" :value="i">Test Case {{ i+1 }}</v-tab>
+                                    </v-tabs>
+                                     <v-window v-model="testcase_tab">
+                                         <v-window-item v-for="(test_case, i) in question.testCases" :key="i" :value="i">
+                                            <v-col>
+                                                <h4 class="ml-3 display-4">n = </h4>
+                                                <v-text-field v-model="inputs[i]"/>
+                                            </v-col>
+                                         </v-window-item>
+                                    </v-window>
+                                    
+                                </v-window-item>
 
-            </div>
-            <div>
-                <button>Run</button>
-                <button>Submit</button>
-            </div>
-        </div>
-    </div>
+                                <v-window-item value="result">
+                                <p class="text-red" v-if="error != '' ">{{ error }}</p>
+                                <div v-else>
+                                     <v-tabs
+                                        dark
+                                        v-model="output_tab"
+                                    >
+                                        <v-tab v-for="(test_case, i) in output" :key="i" :value="i" ><span :style="'color:' + (test_case.status ? '#00ff00' : '#ff0000')">Test Case {{ i+1 }}</span></v-tab>
+                                        </v-tabs>
+                                        <v-window v-model="output_tab">
+                                            <v-window-item v-for="(test_case, i) in output" :key="i" :value="i">
+                                                <v-col>
+                                                     <!-- { "testCase": "0", "answer": "0", "expectedAns": "1", "status": false } -->
+                                                    <p>n = {{ test_case.testCase }}</p>
+                                                    <p>output : {{ test_case.answer }}</p>
+                                                    <p>expected : {{ test_case.expectedAns }}</p>
+                                                </v-col>
+                                            </v-window-item>
+                                        </v-window>
+                                </div>
+                                </v-window-item>
+                            </v-window>
+                            </v-card-text>
+                        </v-card>
+                    </v-row>
+
+                    <v-row>
+                        <v-row class="px-3 mt-4">
+                            <v-btn :disabled="loading" @click="runCode" variant="tonal">
+                                Run
+                            </v-btn>
+                            <v-spacer />
+                             <v-btn :disabled="loading" @click="submit" variant="tonal">
+                                Submit
+                            </v-btn>
+                        </v-row>
+                    </v-row>
+                   </v-col>
+
+
+                </v-card>
+        </v-row>
+    </v-app>
 </template>
 <script>
 import axios from 'axios';
@@ -30,7 +132,15 @@ export default {
         return {
           question: {},
           output: "",
-          inputValue: ""
+          inputValue: "",
+          code_tab: 0,
+          compiler: "jdk8",
+          code: "",
+          testcase_tab: 0,
+          inputs: [],
+          error: "",
+          output_tab: 0,
+          loading: false
         };
     },
     mounted() {
@@ -38,22 +148,33 @@ export default {
     },  
     methods: {
         loadQuestion() {
+            this.loading = true
             axios.get("/api/v1/questions/factorial")
             .then(({data}) => {
+                this.loading = false
                 console.log({data});
                 this.question  = data
+                this.inputs = this.question.testCases.map(i => i.input)
                 this.code = this.question.sampleCodes.filter(i => i.compiler == 'jdk8')[0].code
             })
         },  
         runCode() {
+            this.loading = true
+            this.error = ""
           axios.post("/api/v1/run/questions/factorial", {
             code : this.code,
             compiler: "jdk8",
-            testCases : this.convertTestCase()
+            testCases : this.inputs
           })
             .then(({data}) => {
+              this.code_tab = 1
               console.log({data});
-              this.output = data
+              this.loading = false
+              if(data.error) {
+                this.error = data.error
+              }
+              else 
+                this.output = data
             })
         },
         convertTestCase() {
@@ -76,33 +197,5 @@ export default {
 <style>
 
 body {
-    margin: 0px;
-}
-
-div {
-    border: 1px solid grey;
-}
-
-.question-container {
-    display: flex;
-    flex-direction: row;
-    height: 100vh;
-    margin: 0px;
-}
-
-.question-detail {
-    width: 30%;
-}
-
-.code-container {
-    display: flex;
-    flex-direction: column;
-    width: 70%;
-}
-
-.code-input {
-    width: 100%;
-    padding: 8px;
-    font-size: 14px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+
 axios.defaults.baseURL = "http://127.0.0.1:8020"
-createApp(App).mount('#app')
+createApp(App).use(vuetify).mount('#app')
 
